c008352e32bdecd04f87fbb1720456855b9f1022
Rana saqib <160149771+Saqibs20@users.noreply.github.com>
2024-05-11 20:24:09 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,15 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {
+    message: "Enter your paragraph and to count the words:",
+    type: "input",
+    name: "Sentence",
+  },
+]);
+
+const word = answer.Sentence.trim().split(" ");
+console.log(word);
+console.log(`The total word are ${word.length}`);
+console.log("The End");
